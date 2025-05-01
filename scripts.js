a04be6e360a785800bb882d7897318ddcca3c314
Manuel Capiendo <manuelgcapiendo@gmail.com>
2025-05-01 06:53:15 -0400

    resturucte classes and html and css for modal functionality and better percision for css styling. i literally couldnt sleep thinking of what i was gonna do

--- scripts.js
+++ scripts.js
@@ -1,8 +1,22 @@
+// WelcomeScreen Modal
 const modalContainer = document.getElementById("modal_container");
 const closeButton = document.getElementById("close");
 const gameContainer = document.getElementById("game_container");
 
 closeButton.addEventListener("click", () => {
   modalContainer.style.display = "none";
-  gameContainer.style.display = "block";
+  gameContainer.style.display = "flex";
 });
+// End Welcome Screen Modal
+
+//Settings Modal
+
+//Game Info Modal
+
+//Park1 Game Modal
+
+//Park2 Game Modal
+
+//Park3 Game Modal
+
+//then just display round and counter in each game modal respectiely
