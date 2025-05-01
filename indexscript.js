fb567d3e2ff98f482a168d0b541e0c769b19f387
Manuel Capiendo <manuelgcapiendo@gmail.com>
2025-05-01 19:23:11 -0400

    Added functional modal buttons for: game info and settings. Added new font. On index.html only so far

--- indexscript.js
+++ indexscript.js
@@ -15,3 +15,31 @@ if (visited) {
     sessionStorage.setItem("visited", "true");
   });
 }
+
+const infomodalContainer = document.getElementById("info-container");
+// set the infomodal to be invisible at first
+infomodalContainer.style.display = "none";
+// open the game info modal
+const infoOpenbutton = document.getElementById("infoopen");
+infoOpenbutton.addEventListener("click", () => {
+  infomodalContainer.style.display = "flex";
+});
+// close the game info modal
+const infoClosebutton = document.getElementById("infoclose");
+infoClosebutton.addEventListener("click", () => {
+  infomodalContainer.style.display = "none";
+});
+
+const settingsmodalContainer = document.getElementById("settings-container");
+// set the settings modal to be invisible at first
+settingsmodalContainer.style.display = "none";
+// open the settings modal
+const settingsOpenbutton = document.getElementById("settingsopen");
+settingsOpenbutton.addEventListener("click", () => {
+  settingsmodalContainer.style.display = "flex";
+});
+// close the settings modal
+const settingsClosebutton = document.getElementById("settingsclose");
+settingsClosebutton.addEventListener("click", () => {
+  settingsmodalContainer.style.display = "none";
+});
