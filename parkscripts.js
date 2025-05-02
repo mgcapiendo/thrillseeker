fc7e378c5dad3cfa08a9b5dc2f5f00a516413c17
Manuel Capiendo <manuelgcapiendo@gmail.com>
2025-05-02 18:32:34 -0400

    added update to the settings select to the round counter and timer display

--- parkscripts.js
+++ parkscripts.js
@@ -28,3 +28,62 @@ const settingsClosebutton = document.getElementById("settingsclose");
 settingsClosebutton.addEventListener("click", () => {
   settingsmodalContainer.style.display = "none";
 });
+
+// Start of Difficulty------------------------------------------
+const difficultySelect = document.getElementById("difficulty");
+const timerDisplay = document.querySelector(".timer");
+const settingsCloseButton = document.getElementById("settingsclose");
+
+const difficultySettings = {
+  easy: 20,
+  medium: 10,
+  hard: 5,
+};
+
+let selectedDifficulty = "easy";
+updateTimerDisplay();
+
+function updateTimerDisplay() {
+  const timerValue = difficultySettings[selectedDifficulty];
+  timerDisplay.textContent = `Timer: ${timerValue}s`;
+}
+
+difficultySelect.addEventListener("change", function () {
+  selectedDifficulty = difficultySelect.value;
+  updateTimerDisplay();
+});
+
+settingsCloseButton.addEventListener("click", () => {
+  updateTimerDisplay();
+});
+// End of Difficulty--------------------------------------------
+
+// Start of Rounds----------------------------------------------
+const roundSelect = document.getElementById("round");
+const roundDisplay = document.querySelector(".round");
+
+const roundSettings = {
+  quick: 3,
+  normal: 5,
+  long: 7,
+};
+
+let selectedRounds = "quick";
+updateRoundDisplay();
+
+function updateRoundDisplay() {
+  const roundValue = roundSettings[selectedRounds];
+  roundDisplay.textContent = `Rounds: ${roundValue}`;
+}
+
+roundSelect.addEventListener("change", function () {
+  selectedRounds = roundSelect.value;
+  updateRoundDisplay();
+});
+
+// End of Rounds------------------------------------------------
+
+//
+// Start of Game --------------------------------------------------------
+
+// End Game kek ---------------------------------------------------------
