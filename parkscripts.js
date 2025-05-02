f8d8d4d373ab80ec6c339eca6ef0dcbb559702d1
Manuel Capiendo <manuelgcapiendo@gmail.com>
2025-05-02 15:07:11 -0400

    added settings modal with the drown down menus

--- parkscripts.js
+++ parkscripts.js
@@ -1,2 +1,30 @@
 const gameContainer = document.getElementById("game_container");
 gameContainer.style.display = "flex";
+
+const infomodalContainer = document.getElementById("info-container");
+// set the infomodal to be invisible at first
+infomodalContainer.style.display = "none";
+// open the game info modal
+const infoOpenbutton = document.getElementById("infoopen");
+infoOpenbutton.addEventListener("click", () => {
+  infomodalContainer.style.display = "flex";
+});
+// close the game info modal
+const infoClosebutton = document.getElementById("infoclose");
+infoClosebutton.addEventListener("click", () => {
+  infomodalContainer.style.display = "none";
+});
+
+const settingsmodalContainer = document.getElementById("settings-container");
+// set the settings modal to be invisible at first
+settingsmodalContainer.style.display = "none";
+// open the settings modal
+const settingsOpenbutton = document.getElementById("settingsopen");
+settingsOpenbutton.addEventListener("click", () => {
+  settingsmodalContainer.style.display = "flex";
+});
+// close the settings modal
+const settingsClosebutton = document.getElementById("settingsclose");
+settingsClosebutton.addEventListener("click", () => {
+  settingsmodalContainer.style.display = "none";
+});
